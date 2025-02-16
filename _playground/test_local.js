65e7851ff212ed61663d8a6640b925ac88d2175e
JnJ One <jnjsoft.one@gmail.com>
2025-02-16 16:32:17 +0900

    코드 정리

--- _playground/test_local.js
+++ _playground/test_local.js
@@ -17,24 +17,7 @@ const googleSheets = new GoogleSheets(spreadsheetId, {
   authDir: GOOGLE_AUTH_DIR,
 });
 
-// oauth2_bigwhitekmc_0.json
-// oauth2_bigwhitekmc_0.json
-
-// const spreadsheetId = "14HsVYROe_RDI2zg6C1S8uAYUen4a7wIh0_p7VTtxn-A";
-// const googleSheets = new GoogleSheets(spreadsheetId, {user: 'mooninlearn', type: 'oauth2', scopeDir: GOOGLE_SCOPE_DIR, authDir: GOOGLE_AUTH_DIR});
-
-// const spreadsheetId = "1_TA0wR1R7u32cT_iQBGmaXsi4o5zFSZgV2e-0PNz-Zo"
-// const googleSheets = new GoogleSheets(spreadsheetId, {user: 'moondevgoog', type: 'oauth2', scopeDir: GOOGLE_SCOPE_DIR, authDir: GOOGLE_AUTH_DIR});
 
 const client = await googleSheets.init();
 const names = await googleSheets.getSheetNames();
 console.log(names);
-
-// (async ()=> {
-//   const client = await googleSheets.init();
-//   const names = await googleSheets.getSheetNames();
-//   console.log(names);
-// })();
-
-// const names = await googleSheets.getSheetNames();
-// console.log(names);
